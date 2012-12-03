ba3e551a95b33ead0f74626f6ae5a55424d35696
tekkub :bear:  <tekkub@gmail.com>
2012-12-03 00:00:02 -0700

    Add jewelers setting

--- VendorPrices.lua
+++ VendorPrices.lua
@@ -59,6 +59,7 @@ local prices = [[
 44500 15000000 -- Goblin-machined Piston (for Mechano-hog)
 44501 10000000 -- Goblin-machined Piston (for Mechano-hog)
 44999 30000000 -- Salvaged Iron Golem Parts (for Mechano-hog)
+52188 3750 -- Jeweler's Setting
 ]]
 
 
