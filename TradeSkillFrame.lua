ed192f77dbb9bce83c28a1a1d8d7e0dbc2dfde7f
tekkub :bear:  <tekkub@gmail.com>
2014-12-07 02:23:27 -0700

    Add cost per combine to the recipe list

--- TradeSkillFrame.lua
+++ TradeSkillFrame.lua
@@ -26,42 +26,127 @@ local function GetNumMade(index, id)
 end
 
 
+local function GetReagentCost(id)
+	local cost, incomplete = 0
+	for i=1,GetTradeSkillNumReagents(id) do
+		local link = ns.GetTradeSkillReagentItemLink(id, i)
+		if link then
+			local _, _, count = GetTradeSkillReagentInfo(id, i)
+			local itemid = tonumber((string.match(link, "item:(%d+):")))
+			local price = ns.GetPrice(itemid)
+			cost = cost + (price or 0) * count
+			if not price then incomplete = true end
+		else incomplete = true end
+	end
+
+	local _, skillType, _, _, _, numSkillUps = GetTradeSkillInfo(id)
+	if incomplete then
+		TradeSkillReagentLabel:SetText(SPELL_REAGENTS.." Incomplete price data")
+	else
+		if skillType == "optimal" and numSkillUps > 1 then
+			TradeSkillReagentLabel:SetText(
+				SPELL_REAGENTS.." "..ns.GS(cost)..
+				" - "..ns.GS(cost / numSkillUps).. " per skillup"
+			)
+		else
+			TradeSkillReagentLabel:SetText(SPELL_REAGENTS.." ".. ns.GS(cost))
+		end
+	end
+
+	if not incomplete then
+		local link = GetTradeSkillItemLink(id)
+		local itemid = link and tonumber((string.match(link, "item:(%d+):")))
+		if itemid then
+			ns.combineprices[itemid] = cost / (GetNumMade(id, itemid) or 1)
+		end
+	end
+
+	return cost, incomplete
+end
+
+
+local frames = {}
+local function GetCostFrame(i)
+	if frames[i] then return frames[i] end
+
+	local butt = _G["TradeSkillSkill"..i]
+	local f = butt:CreateFontString(nil, "ARTWORK", "GameFontNormalSmall")
+	f:SetPoint("LEFT", 2, 0)
+	frames[i] = f
+	return f
+end
+
+
+local function UpdateList()
+	local offset = FauxScrollFrame_GetOffset(TradeSkillListScrollFrame)
+
+	local filtered = TradeSkillFilterBar:IsShown()
+	local numShown = TRADE_SKILLS_DISPLAYED - (filtered and 1 or 0)
+
+	for i=1,numShown do
+		local skillIndex = i + offset
+		local _, skillType = GetTradeSkillInfo(skillIndex)
+
+		local buttonIndex = i + (filtered and 1 or 0)
+		local text = GetCostFrame(buttonIndex)
+
+		if skillType == "header" or skillType == "subheader" then
+			text:Hide()
+		else
+			local cost, incomplete = GetReagentCost(skillIndex)
+			if incomplete then
+				text:SetText(GRAY_FONT_COLOR_CODE.."--")
+			else
+				text:SetText(ns.GS(cost))
+			end
+			text:Show()
+		end
+	end
+
+end
+
+
+local function UpdateDetailFrame()
+	local id = GetTradeSkillSelectionIndex()
+	local cost, incomplete = 0
+	for i=1,GetTradeSkillNumReagents(id) do
+		local link = ns.GetTradeSkillReagentItemLink(id, i)
+		if link then
+			local _, _, count = GetTradeSkillReagentInfo(id, i)
+			local itemid = tonumber((string.match(link, "item:(%d+):")))
+			local price = ns.GetPrice(itemid)
+			cost = cost + (price or 0) * count
+			if not price then incomplete = true end
+		else incomplete = true end
+	end
+
+	local _, skillType, _, _, _, numSkillUps = GetTradeSkillInfo(id)
+	if incomplete then
+		TradeSkillReagentLabel:SetText(SPELL_REAGENTS.." Incomplete price data")
+	else
+		if skillType == "optimal" and numSkillUps > 1 then
+			TradeSkillReagentLabel:SetText(
+				SPELL_REAGENTS.." "..ns.GS(cost)..
+				" - "..ns.GS(cost / numSkillUps).. " per skillup"
+			)
+		else
+			TradeSkillReagentLabel:SetText(SPELL_REAGENTS.." ".. ns.GS(cost))
+		end
+	end
+
+	if not incomplete then
+		local link = GetTradeSkillItemLink(id)
+		local itemid = link and tonumber((string.match(link, "item:(%d+):")))
+		if itemid then
+			ns.combineprices[itemid] = cost / (GetNumMade(id, itemid) or 1)
+		end
+	end
+end
+
 local function HookTradeSkill()
 	hooksecurefunc("TradeSkillFrame_Update", function()
-		local id = GetTradeSkillSelectionIndex()
-		local cost, incomplete = 0
-		for i=1,GetTradeSkillNumReagents(id) do
-			local link = ns.GetTradeSkillReagentItemLink(id, i)
-			if link then
-				local _, _, count = GetTradeSkillReagentInfo(id, i)
-				local itemid = tonumber((string.match(link, "item:(%d+):")))
-				local price = ns.GetPrice(itemid)
-				cost = cost + (price or 0) * count
-				if not price then incomplete = true end
-			else incomplete = true end
-		end
-
-		local _, skillType, _, _, _, numSkillUps = GetTradeSkillInfo(id)
-		if incomplete then
-			TradeSkillReagentLabel:SetText(SPELL_REAGENTS.." Incomplete price data")
-		else
-			if skillType == "optimal" and numSkillUps > 1 then
-				TradeSkillReagentLabel:SetText(
-					SPELL_REAGENTS.." "..ns.GS(cost)..
-					" - "..ns.GS(cost / numSkillUps).. " per skillup"
-				)
-			else
-				TradeSkillReagentLabel:SetText(SPELL_REAGENTS.." ".. ns.GS(cost))
-			end
-		end
-
-		if not incomplete then
-			local link = GetTradeSkillItemLink(id)
-			local itemid = link and tonumber((string.match(link, "item:(%d+):")))
-			if itemid then
-				ns.combineprices[itemid] = cost / (GetNumMade(id, itemid) or 1)
-			end
-		end
+		UpdateList()
+		UpdateDetailFrame()
 	end)
 end
 
