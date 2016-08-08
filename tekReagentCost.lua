a8424e0128b81ac770cad6a7e98c5003c75cf7a2
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-08-08 13:59:34 -0600

    Blacklist some old transmutes from cost calc

--- tekReagentCost.lua
+++ tekReagentCost.lua
@@ -1,11 +1,14 @@
 
 local myname, ns = ...
 
-
+local ah_only = [[
+	21884 21885 22452 22451 21886 22456 22457
+]]
 function ns.GetPrice(itemID)
 	if not itemID then return end
 	if ns.vendor[itemID] then return ns.vendor[itemID], -1 end
 	local price = GetAuctionBuyout and GetAuctionBuyout(itemID)
+	if ah_only:match(itemID) then return price, 1 end
 	local craftedprice = ns.combineprices[itemID]
 	if price and craftedprice then return math.min(price, craftedprice), 1 end
 	return price or craftedprice, 1
